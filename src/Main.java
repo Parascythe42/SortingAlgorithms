a471883ad99c43fc59025d78db2bd382006c92f9
Matthew Fox <25204673+Parascythe42@users.noreply.github.com>
2024-01-26 13:30:36 -0400

    Made an array of random integers

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int range = 10000;
+        int length = 100;
+        int[] unsorted = new int[length];
+        int[] sorted = new int[length];
+        Random random = new Random();
+
+        BubbleSort bubbleSort = new BubbleSort();
+
+        for (int i = 0; i < unsorted.length; i++) {
+            unsorted[i] = random.nextInt(range);
+        }
+
+        sorted = bubbleSort.sort(unsorted);
     }
 }
