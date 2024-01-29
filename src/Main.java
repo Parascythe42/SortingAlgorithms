0a5a494cbe4e9945a5eebcf3cef3190a4c6b8dbe
Matthew Fox <25204673+Parascythe42@users.noreply.github.com>
2024-01-29 16:28:07 -0400

    Added some comments

--- src/Main.java
+++ src/Main.java
@@ -3,23 +3,27 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
+//        Variable definitions.
         int range = 10000;
         int length = 100;
         int[] unsorted = new int[length];
         int[] sorted;
         Random random = new Random();
-
         Sorter sorter = new Sorter();
 
+//        Fills the array with random values.
         for (int i = 0; i < unsorted.length; i++) {
             unsorted[i] = random.nextInt(range);
         }
 
+//        Sorts the array and stores it in another variable.
 //        sorted = sort.bubble(unsorted);
 //        sorted = sort.insertion(unsorted);
         sorted = sorter.selection(unsorted);
 
+//        Prints the array to the console.
         System.out.println(Arrays.toString(sorted));
-        System.out.println(sorter.verify(sorted));
+//        Prints if the array is sorted or not.
+        System.out.println(sorter.verify(sorted) ? "The array is sorted.\uD83D\uDC4D" : "The array is not sorted.\uD83D\uDC4E");
     }
 }
