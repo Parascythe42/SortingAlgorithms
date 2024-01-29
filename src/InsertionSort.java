9f02052fcedcd9fd52e70a1f9fb8cbb19a62ec23
Matthew Fox <25204673+Parascythe42@users.noreply.github.com>
2024-01-29 15:24:50 -0400

    Changed array name

--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -2,35 +2,35 @@ public class InsertionSort {
     /**
      * Uses Insertion Sort to sort an array of integers, lowest to highest.
      *
-     * @param unsorted An unsorted array of integers
+     * @param ints An unsorted array of integers
      * @return A sorted version of the input array
      */
-    public int[] sort(int[] unsorted) {
+    public int[] sort(int[] ints) {
 //        Defining variables.
         int key;
         int j;
 
 //        Iterates over the entire array from the second item.
-        for (int i = 1; i < unsorted.length; i++) {
+        for (int i = 1; i < ints.length; i++) {
 //            Stores the current value to move later.
-            key = unsorted[i];
+            key = ints[i];
 //            Initializing another iterator.
             j = i - 1;
 
 //            Starts from the previous item and checks if the current item is less than it, continues moving the
 //            current item backwards through the array until that's false.
-            while (j >= 0 && key < unsorted[j]) {
+            while (j >= 0 && key < ints[j]) {
 //                Sets the next item equal to the previous.
-                unsorted[j + 1] = unsorted[j];
+                ints[j + 1] = ints[j];
 //                Decrements the second iterator.
                 j--;
             }
 //            Sets the next item, the one that was just moved ahead, equal to the current value that was stored, so it
 //            can be compared to the next previous item.
-            unsorted[j + 1] = key;
+            ints[j + 1] = key;
         }
 
 //        Returns the sorted array.
-        return unsorted;
+        return ints;
     }
 }
