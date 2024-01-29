9417a368f05e96161ed42c8fd04c1cf7a71e0348
Matthew Fox <25204673+Parascythe42@users.noreply.github.com>
2024-01-29 15:40:18 -0400

    Added selection sort

--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -1,5 +1,27 @@
 public class SelectionSort {
     public int[] sort(int[] unsorted) {
+
+        int min;
+        int minIndex = 0;
+//        int max = Integer.MIN_VALUE;
+
+        int j;
+
+        for (int i = 0; i < unsorted.length; i++) {
+            j = i;
+            min = Integer.MAX_VALUE;
+            while (j < unsorted.length) {
+                if (unsorted[j] < min) {
+                    min = unsorted[j];
+                    minIndex = j;
+                }
+
+                j++;
+            }
+            unsorted[minIndex] = unsorted[i];
+            unsorted[i] = min;
+        }
+
         return unsorted;
     }
 }
